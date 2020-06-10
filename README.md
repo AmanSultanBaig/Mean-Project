61df2ecd08621fd4ab2f218c1b47b23759251b89
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-06-10 18:34:19 +0500

    login api implemented and tested

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Mean-Project
+
+### Overview 
+This application is created for those who want to know that how to write well scalable code using [MeanStack], so this is the public repository for the students and other developers who want to see that how I write code, for using this project in your local system you need to come with following steps.
+
+### Step 1
+After clone completed go to server (Mean-Project > server) using `cd server` & run `npm i` or `npm install`, after installing all important modules which are used to making this application, simple run `npm start`, If you successfully followed these steps so your [Node] project will run. 
+
--- server/routes/register.js
+++ server/routes/register.js
@@ -4,8 +4,9 @@ const router = express.Router()
 // require schema
 const registerModel = require('../models/register.model')
 
+// registration api
 router.post('/register', (req, res) => {
-    let register = new registerModel({
+    new registerModel({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
@@ -19,6 +20,42 @@ router.post('/register', (req, res) => {
         .catch(error => {
             res.status(404).json({
                 message: "Registered Not Successfully!"
+            })
+        })
+})
+
+// login api
+router.post('/login', (req, res) => {
+    // extractting email, password
+    let email = req.body.email;
+    let password = req.body.password;
+
+    // matching email for getting exact result
+    registerModel.findOne({ email: email })
+        .then(userData => {
+            // checking email
+            if (!userData) {
+                res.status(401).json({
+                    message: "Invalid Email"
+                })
+            }
+            // checking password
+            else if (userData.password !== password) {
+                res.status(401).json({
+                    message: "Invalid Password"
+                })
+            } else {
+                // login successfully after checking
+                res.status(200).json({
+                    message: "Login Successfully!",
+                    result: userData
+                })
+            }
+        })
+        .catch(err => {
+            // catching error if there is any
+            res.status(404).json({
+                message: "Not Found"
             })
         })
 })
